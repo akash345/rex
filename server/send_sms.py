68c975345717898921aa50b29b9dbb01e405cfef
Akash Mandavilli <akashmandavilli@gmail.com>
2020-08-12 17:50:03 -0400

    ENV variables

--- server/send_sms.py
+++ server/send_sms.py
@@ -2,15 +2,10 @@
 from twilio.rest import Client
 
 
-# Your Account Sid and Auth Token from twilio.com/console
-# DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC8765801a6f4501d32f3d6a2bc28e31cd'
-auth_token = '9a72e3f1833b400e1f382a3cf1051906'
-client = Client(account_sid, auth_token)
 
 def send_sms(phone_number, message):
-    account_sid = 'AC8765801a6f4501d32f3d6a2bc28e31cd'
-    auth_token = '9a72e3f1833b400e1f382a3cf1051906'
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
     client = Client(account_sid, auth_token)
 
     numbers_to_message = ['+16789069312']
