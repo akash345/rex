163bf2f8347efc7e55bb45e5849bfacde5438881
Vikram Ruppa-Kasani <vikram@Vikrams-MacBook-Pro.local>
2020-08-12 20:39:42 -0400

    successfulUserStorage

--- UI/Rex_Extension/mainRex.js
+++ UI/Rex_Extension/mainRex.js
@@ -5,15 +5,18 @@ alert("reached")
 // alert(friend1.firstname);
 
 var friend1 = document.getElementById("friend1");
-var contact1data = chrome.storage.sync.get("contact1", function (contact) {
-      alert(contact.firstname);
-      var contact = JSON.stringify(contact);
-      alert(contact.firstname);
-      friend1.innerHTML = contact.firstname;
+var contact1data = chrome.storage.sync.get(["contact1", "contact2" , "contact3"], function (result) {
+      var contact1 = JSON.parse(result.contact1);
+      var contact2 = JSON.parse(result.contact2);
+      var contact3 = JSON.parse(result.contact3);
+
+      friend1.innerHTML = contact1.firstname + " " + contact1.lastname;
+      friend2.innerHTML = contact2.firstname + " " + contact2.lastname;
+      friend3.innerHTML = contact3.firstname + " " + contact3.lastname;
+
+      let sendButtons = document.getElementsByClassName("button");
+
+      for (i = 0; i < sendButtons.length; i++) {
+          sendButtons[i].addEventListener("click", sendText);
+      }
     });
-
-
-var friends = document.querySelectorAll("content");
-for(var i = 0; i < friends.length; i++){
-    //friends[i].innerHTML = chrome.storage.get('user');
-}
